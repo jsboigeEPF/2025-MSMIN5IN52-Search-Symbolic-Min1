378f9efb48d0a85264cea1b765324d413e452f9c
Sebastien Dors <dorssebastien@gmail.com>
2025-12-03 00:34:18 +0100

    Generate Angular components and services for the frontend

---
+++ DEBAT_AI/frontend/src/app/pages/debate-view/debate-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-debate-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './debate-view.component.html',
+  styleUrl: './debate-view.component.css'
+})
+export class DebateViewComponent {
+
+}
---
+++ DEBAT_AI/frontend/src/app/pages/topics-list/topics-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topics-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './topics-list.component.html',
+  styleUrl: './topics-list.component.css'
+})
+export class TopicsListComponent {
+
+}
